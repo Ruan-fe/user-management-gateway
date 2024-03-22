4a96bab2c2f64e0f1c4bb0404cebec6edcf52512
Ruan Felipe Totti do Amaral <ruanfelipetottiamaral@gmail.com>
2024-03-22 00:43:06 -0300

    first commit

---
+++ src/test/java/com/usermanagementgateway/UserManagementGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanagementgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
